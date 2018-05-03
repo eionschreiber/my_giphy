dd508d2e04982ab38dc570dafa01eddc5526dc6a
eionschreiber <eionschreiber@gmail.com>
2018-05-02 22:47:18 -0500

    Connected to Giphy API and displays 30 images to the site.
    Created initial structure using HTML Boilerplate and connected to Giphy
    API.

---
+++ dist/js/main.js
@@ -0,0 +1,20 @@
+//javascript, jQuery
+
+function getData(){
+
+    var input = $("#searchtext").val()
+
+    var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=" + input + "&api_key=jSPmjWAflCKQgsYNIYsb2NPnXB4DURdE&limit=30");
+    xhr.done(function(response) {
+         console.log("success got data", response);
+    
+    var jiffs = response.data;
+    
+    for (i in jiffs)
+    {
+        $('.page-wrap').append("<img src='" + jiffs[i].images.original.url + "'/>")
+    }
+    });
+
+}
+
