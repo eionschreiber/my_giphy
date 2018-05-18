d045935dae40f8300d2f6be65521d569715b4aa8
eionschreiber <eionschreiber@gmail.com>
2018-05-17 19:03:53 -0500

    Changed from CSS Grids to Flexbox and added messy js code
    I changed from using css grids to flexbox based on the look of the
    gallery being created with css grids. If I had a set of images that
    were not random then this would be much easier and I would be able to
    use css grids. This is not the case though and I will be making the
    image gallery similar to that of Pinterest with the staggered images
    based on height. I also cut the array being called from the Giphy API
    into three different variables and then ran a for loop to sort them
    into three columns. This was just a quick fix to test the flexbox
    columns.

--- js/main.js
+++ js/main.js
@@ -5,16 +5,47 @@ function getData(){
     var input = $("#searchtext").val()
 
     var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=" + input + "&api_key=jSPmjWAflCKQgsYNIYsb2NPnXB4DURdE&limit=30");
+    
     xhr.done(function(response) {
          console.log("success got data", response);
     
     var jiffs = response.data;
-    
-    for (i in jiffs)
-    {
-        $('.grid-container').append("<div class='grid-item " + i +"'><img src='" + jiffs[i].images.original.url + "'/></div>")
+    console.log(jiffs);
+    console.log(jiffs[1]);
+    console.log(jiffs.length);
+    console.log(typeof jiffs);
+
+    var jiffsCutOne = jiffs.slice(0, 9);
+    var jiffsCutTwo = jiffs.slice(10, 19);
+    var jiffsCutThree = jiffs.slice(20, 30);
+    console.log(jiffsCutOne);
+
+    function sliceArray(){
+
+
     }
+    for(i in jiffsCutOne)
+    {
+        $('.column-one').append("<img src='" + jiffsCutOne[i].images.original.url + "'/>");
+    }
+    for(i in jiffsCutTwo)
+    {
+        $('.column-two').append("<img src='" + jiffsCutTwo[i].images.original.url + "'/>");
+    }
+    for(i in jiffsCutThree)
+    {
+        $('.column-three').append("<img src='" + jiffsCutThree[i].images.original.url + "'/>");
+    }
+
+
+    // for (i in jiffs)
+    // {
+    //     $('.column-one').append("<img src='" + jiffs[i].images.original.url + "'/>");
+    //     console.log(i);
+    // }
     });
 
 }
+
+
 
